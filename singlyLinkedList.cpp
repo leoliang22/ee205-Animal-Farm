3ecf9c0b88738c4b799e063aafcaca46746d7cf2
Leo Liang <leoliang@hawaii.edu>
2022-04-19 18:16:00 -1000

    Added basic SinglyLinkedList

---
+++ singlyLinkedList.cpp
@@ -0,0 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  _ee205_lab_08d_animal_farm_1_to_clion - EE 205 - Spr 2022
+///
+/// @file singlyLinkedList.cpp
+/// @version 1.0
+///
+/// @author Leo Liang <leoliang@hawaii.edu>
+/// @date   19_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#include "singlyLinkedList.h"
+
---
+++ singlyLinkedList.h
@@ -0,0 +1,28 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  _ee205_lab_08d_animal_farm_1_to_clion - EE 205 - Spr 2022
+///
+/// @file singlyLinkedList.h
+/// @version 1.0
+///
+/// @author Leo Liang <leoliang@hawaii.edu>
+/// @date   19_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#include "List.h"
+
+#ifndef _EE205_LAB_08D_ANIMAL_FARM_1_TO_CLION_SINGLYLINKEDLIST_H
+#define _EE205_LAB_08D_ANIMAL_FARM_1_TO_CLION_SINGLYLINKEDLIST_H
+
+#endif //_EE205_LAB_08D_ANIMAL_FARM_1_TO_CLION_SINGLYLINKEDLIST_H
+
+class SinglyLinkedList:List{
+public:
+    SinglyLinkedList ();
+    void 	push_front (Node *newNode);
+    Node * 	pop_front () noexcept override;
+    void 	insert_after (Node *currentNode, Node *newNode);
+    void 	dump () const noexcept override;
+    bool 	validate () const noexcept override;
+
+};
