c5dbab7e11c07746df3be021b433f5e6c90a29b9
Leo Liang <leoliang@hawaii.edu>
2022-04-19 17:09:54 -1000

    Added the macros for print dumping

--- config.h
+++ config.h
@@ -12,3 +12,19 @@
 
 #define MAX_CATS 1024
 #define PROGRAM_NAME "Animal Farm 3"
+
+/// Format the heading for dumping members of a class to the console
+///
+/// Print =====================
+#define PRINT_HEADING_FOR_DUMP \
+/* Print =========================================================== */ \
+std::cout << std::setw(80) << std::setfill( '=' ) << "" << std::endl
+/// Format a line for dumping the members of a class to the console.
+/// Setup the fields for printing (space pad, left justify, etc.)
+#define FORMAT_LINE_FOR_DUMP( className, member ) \
+std::cout << std::setfill( ' ' ) /* Space pad */ \
+<< std::left /* Left justify */ \
+<< std::boolalpha /* Print `true` or `false` for `bool`s */ \
+<< std::setw(8) << (className) \
+<< std::setw(20) << (member) \
+<< std::setw(52) /* (data) */
